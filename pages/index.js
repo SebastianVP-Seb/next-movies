b4bfa9a94eceee699e1b89b519809a4040722f1c
SebastianVP-Seb <palacios.sebastian.24@outlook.com>
2021-12-02 18:20:13 -0600

    Se agrega la etiquedata HTML a index.js

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,21 @@ import 'antd/dist/antd.css';
 import { Button } from 'antd';
 import Layout from '../components/Layout'
 import Slider from '../components/Slider';
+import {Html} from 'next/document';
 
 export default function Home() {
   return (
-    <Layout >
-    {/* <Button type="primary" danger>
-      Primary
-    </Button> */}
+    <Html>
 
-    <Slider />
+      <Layout >
+      {/* <Button type="primary" danger>
+        Primary
+      </Button> */}
 
-    </Layout>
+        <Slider />
+
+      </Layout>
+
+    </Html>
   )
 }
